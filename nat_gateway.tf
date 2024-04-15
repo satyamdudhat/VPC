c760d58a7f2196825878463af57f8afb1ac43ae8
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-15 14:06:17 +0530

    intial commit

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -1,4 +1,5 @@
 resource "aws_nat_gateway" "nat_gateway" {
+    depends_on = [ aws_eip.eip ]
     allocation_id = aws_eip.eip.id
     subnet_id = aws_subnet.vpc_public_subnet[0].id
     tags = {
---
+++ route_table.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+  tags = {
+    Name = "This is an public route table"
+  }
+}
+
+
+
+resource "aws_route_table" "private_route_table" {
+  count = length(var.private_subnets_cidr)
+  vpc_id = aws_vpc.vpc.id
+  depends_on = [ aws_nat_gateway.nat_gateway ]
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+   tags = {
+    Name = "This is an private route table"
+  }
+}
