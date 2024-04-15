c2491af45f117b99aefa8a86c9889d69d620d3a5
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-15 18:12:50 +0530

    intial commit

--- security_group.tf
+++ security_group.tf
@@ -1,23 +1,22 @@
 resource "aws_security_group" "security_group_vpc_Instance" {
-  egress = [{
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1" // Allow all outbound traffic
     cidr_blocks = ["0.0.0.0/0"]
-    self = false
-    to_port = 0
-  }]
-    ingress = [{
+  }
+  
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] // Allowing HTTP from any IP
-    self = false
-  }]
+    cidr_blocks = ["0.0.0.0/0"] // Allowing SSH from any IP
+  }
+
   vpc_id = aws_vpc.vpc.id
-  depends_on = [ aws_vpc.vpc ]
+  depends_on = [aws_vpc.vpc]
 
   tags = {
-    Name = "vpc_instance"
+    Name = "vpc_instance_security_group"
   }
-}   
+}
