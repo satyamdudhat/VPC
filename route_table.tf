692026764938036e3417e993c496a3699a691aa3
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-15 15:38:52 +0530

    intial commit

--- route_table.tf
+++ route_table.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0"                                                                                                                                                                                                                                                                                                    
     gateway_id = aws_internet_gateway.gateway.id
   }
   tags = {
@@ -9,11 +9,11 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table_association" "public_subnet" {
+resource "aws_route_table_association" "public_subnet" {                                            
     count = length(var.public_subnets_cidr)
     depends_on = [ aws_subnet.vpc_public_subnet.id, aws_route_table.public_route_table.id ]
     subnet_id = element(aws_subnet.vpc_public_subnet[*].id, count.index)
-    route_table_id = aws_route_table.public_route_table[count.index].id
+    route_table_id = aws_route_table.public_route_table.id
 }
 
 
