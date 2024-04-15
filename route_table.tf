532d172d3a96561b3c25aa70d62b517a1ff005aa
satyamdudhat <satyam.glsica19@gmail.com>
2024-04-15 14:39:49 +0530

    intial commit

--- route_table.tf
+++ route_table.tf
@@ -9,7 +9,27 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+resource "aws_route_table_association" "public_subnet" {
+    count = length(var.public_subnets_cidr)
+    depends_on = [ aws_subnet.vpc_public_subnet.id, aws_route_table.public_route_table.id ]
+    subnet_id = element(aws_subnet.vpc_public_subnet[*].id, count.index)
+    route_table_id = aws_route_table.public_route_table[count.index].id
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------------Private-----------------------------------------------
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
